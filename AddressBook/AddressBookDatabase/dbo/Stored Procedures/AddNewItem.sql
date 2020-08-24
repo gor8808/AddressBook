9726b22aab53816e1be4663bb220e84f96d4735a
Gor Grigoryan <gor8808@users.noreply.github.com>
2020-08-24 14:18:25 +0400

    Add AddressBookDatabase project in solution

---
+++ AddressBook/AddressBookDatabase/dbo/Stored Procedures/AddNewItem.sql
@@ -0,0 +1,15 @@
+﻿-- =============================================
+-- Author:		Gor Grigoryan
+-- Create date: 19.08.2020
+-- Description:	Add new item
+-- =============================================
+CREATE PROCEDURE AddNewItem 
+ @FullName NVARCHAR (500),
+ @Email NVARCHAR (MAX),
+ @PhoneNumber NVARCHAR (1000),
+ @PhysicalAddress NVARCHAR (MAX)
+AS
+BEGIN
+	INSERT INTO dbo.Addresses(FullName, Email, PhoneNumber, PhysicalAddress)
+	VALUES(@FullName, @Email, @PhoneNumber, @PhysicalAddress)
+END
---
+++ AddressBook/AddressBookDatabase/dbo/Stored Procedures/SearchItem.sql
@@ -0,0 +1,19 @@
+﻿-- =============================================
+-- Author:		Gor Grigoryan
+-- Create date: 19.08.2020
+-- Description:	Search item
+-- =============================================
+CREATE PROCEDURE SearchItem
+ @FullName NVARCHAR (500),
+ @Email NVARCHAR (MAX),
+ @PhoneNumber NVARCHAR (1000),
+ @PhysicalAddress NVARCHAR (MAX)
+AS
+BEGIN
+	SELECT Id, FullName, Email, PhoneNumber, PhysicalAddress 
+	FROM dbo.Addresses
+	WHERE FullName LIKE '%'+ @FullName +'%' AND 
+	Email LIKE '%'+ @Email +'%' AND
+	PhoneNumber LIKE '%'+ @PhoneNumber +'%' AND
+	PhysicalAddress LIKE '%'+ @PhysicalAddress +'%';
+END
